40a1ea0e31b7b109e043adec1766b4e9dda5c5ac
Ernesto Alamilla <gifted.orphan@gmail.com>
2016-09-25 13:25:01 -0430

    Mofify table tr click

--- app/assets/javascripts/table_tr_click.js
+++ app/assets/javascripts/table_tr_click.js
@@ -1,14 +1,9 @@
 $(document).on('turbolinks:load', function () {
   // Make table rows clickable
-  $(".table tr").on('click', function(event) {
+  $(".table tbody>tr").on('click', function(event) {
     var target = $(event.target);
     if (target.is(":not(a)")) {
       window.location = $(this).data("link")
-    }
-    else if (target.text() == "Imagenes"){
-    }
-    else {
-      var href = $(this).find("a").attr("href");
     }
   });
 });
