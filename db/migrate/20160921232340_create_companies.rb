17aa1a88569aa4c47f62e0bf49938dde0a2d2b1c
Ernesto Alamilla <gifted.orphan@gmail.com>
2016-09-25 13:28:17 -0430

    Modify schema and companies migration

--- db/migrate/20160921232340_create_companies.rb
+++ db/migrate/20160921232340_create_companies.rb
@@ -5,5 +5,7 @@ class CreateCompanies < ActiveRecord::Migration[5.0]
 
       t.timestamps
     end
+
+    add_index :companies, :name,                unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160921232436) do
+ActiveRecord::Schema.define(version: 20160924203855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,13 +22,30 @@ ActiveRecord::Schema.define(version: 20160921232436) do
     t.string   "last_name",  null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "slug"
     t.index ["dni"], name: "index_clients_on_dni", unique: true, using: :btree
+    t.index ["slug"], name: "index_clients_on_slug", unique: true, using: :btree
   end
 
   create_table "companies", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "slug"
+    t.index ["name"], name: "index_companies_on_name", unique: true, using: :btree
+    t.index ["slug"], name: "index_companies_on_slug", unique: true, using: :btree
+  end
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
+    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
+    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
   end
 
   create_table "insurances", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
@@ -39,6 +56,8 @@ ActiveRecord::Schema.define(version: 20160921232436) do
     t.date     "due_date",    null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "slug"
+    t.index ["slug"], name: "index_insurances_on_slug", unique: true, using: :btree
   end
 
   create_table "users", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
