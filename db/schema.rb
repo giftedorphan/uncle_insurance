b7108ae68dc279fd603a9d721f079430c5e1ff14
Ernesto Alamilla <gifted.orphan@gmail.com>
2016-09-27 00:57:25 -0430

    Minor changes to views and added title class for some p tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160924203855) do
+ActiveRecord::Schema.define(version: 20160927020109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,9 @@ ActiveRecord::Schema.define(version: 20160924203855) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "slug"
+    t.boolean  "payment"
+    t.text     "comments"
+    t.index ["payment"], name: "index_insurances_on_payment", using: :btree
     t.index ["slug"], name: "index_insurances_on_slug", unique: true, using: :btree
   end
 
