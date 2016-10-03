023b1f235746aa02c1f5babba5048406e41a9cc3
Ernesto Alamilla <gifted.orphan@gmail.com>
2016-10-02 23:44:43 -0430

    Added pdfkit config options

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,10 +1,12 @@
 PDFKit.configure do |config|
   config.default_options = {
+    :encoding => 'UTF-8',
     :page_size => 'A4',
     :orientation => 'Landscape',
-    :margin_top => '0cm',
-    :margin_right => '0cm',
-    :margin_bottom => '0cm',
+    :footer_right => "Page [page] of [toPage]",
+    :margin_top => '2cm',
+    :margin_right => '1cm',
+    :margin_bottom => '2cm',
     :margin_left => '0cm',
     :print_media_type => true
   }
