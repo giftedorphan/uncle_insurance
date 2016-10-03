951b720f546a9541e884b31b730fc35ffa34059c
Ernesto Alamilla <gifted.orphan@gmail.com>
2016-10-02 21:50:34 -0430

    Change per_page opt to list all or 10

--- app/controllers/insurances_controller.rb
+++ app/controllers/insurances_controller.rb
@@ -4,7 +4,7 @@ class InsurancesController < BaseController
 
   def index
     if params[:date]
-      @insurances = Insurance.search(params[:date]).joins(:client).merge(Client.order(first_name: :asc)).paginate(:page => params[:page], :per_page => 10)
+      @insurances = Insurance.search(params[:date]).joins(:client).merge(Client.order(first_name: :asc)).paginate(:page => params[:page], :per_page => params[:per_page] || 10)
     else
       @insurances = current_client.insurances.joins(:client).merge(Client.order(first_name: :asc)).paginate(:page => params[:page], :per_page => 10)
     end
